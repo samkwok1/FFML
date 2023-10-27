55c6da49ab9cc3e30a382822cb4e9a36aaa8f222
Sam Kwok <samkwok1836@gmail.com>
2023-10-27 16:39:55 -0700

    initial plotting, gda, unification

--- src/experiments/gda.py
+++ src/experiments/gda.py
@@ -1,11 +1,95 @@
 import sys
 import numpy as np
 
-sys.path.append('/Users/jonathan/cs229_final_project/FFML')
+sys.path.append('/Users/Sam/Desktop/FFML')
 
 from src.util import dataprocessing as dp
+from src.util import plots
 
 
-rb_train_path = "all_rb_stats.csv"
+rb_train_path = "src/input_data/RBs/all_rb_stats.csv"
+te_train_path = "src/input_data/tight_ends/all_te_stats.csv"
+wr_train_path = "src/input_data/wrs/all_wr_stats.csv"
 
-x_train, y_train, x_valid, y_valid, x_test, y_test = dp.load_dataset(rb_train_path)
+def main(save_path):
+    """Problem: Gaussian discriminant analysis (GDA)
+
+    Args:
+        train_path: Path to CSV file containing dataset for training.
+        valid_path: Path to CSV file containing dataset for validation.
+        save_path: Path to save predicted probabilities using np.savetxt().
+    """
+    # Load dataset
+    x_train, y_train, x_valid, y_valid, x_test, y_test = dp.load_dataset(te_train_path, 'te')
+    clf = GDA()
+    clf.fit(x_train, y_train)
+    predictions = clf.predict(x_valid)
+    plots.plot(x_test, y_test, clf.theta, 'GDA.png')
+    plots.plot_with_pca(x_test, y_test, clf.theta, 'GDA.png')
+    plots.plot_all_feature_pairs(x_test, y_test, clf.theta, 'GDA.png')
+    np.savetxt(save_path, predictions)
+
+
+class GDA:
+    """Gaussian Discriminant Analysis.
+
+    Example usage:
+        > clf = GDA()
+        > clf.fit(x_train, y_train)
+        > clf.predict(x_eval)
+    """
+    def __init__(self, step_size=0.01, max_iter=10000, eps=1e-5,
+                 theta_0=None, verbose=True):
+        """
+        Args:
+            step_size: Step size for iterative solvers only.
+            max_iter: Maximum number of iterations for the solver.
+            eps: Threshold for determining convergence.
+            theta_0: Initial guess for theta. If None, use the zero vector.
+            verbose: Print loss values during training.
+        """
+        self.theta = theta_0
+        self.step_size = step_size
+        self.max_iter = max_iter
+        self.eps = eps
+        self.verbose = verbose
+
+    def fit(self, x, y):
+        """Fit a GDA model to training set given by x and y by updating
+        self.theta.
+
+        Args:
+            x: Training example inputs. Shape (n_examples, dim).
+            y: Training example labels. Shape (n_examples,).
+        """
+        # *** START CODE HERE ***
+        x_1 = x[y.flatten() == 1]
+        x_0 = x[y.flatten() == 0]
+        phi_1, phi_0 = x_1.shape[0] / x.shape[0], x_0.shape[0] / x.shape[0]
+        mu_1, mu_0 = np.mean(x_1, axis=0), np.mean(x_0, axis=0)
+        Sigma = np.zeros((x.shape[1], x.shape[1]))
+
+        for i in range(x.shape[0]):
+            mu = mu_1 if y[i] == 1 else mu_0
+            Sigma += (np.outer((x[i] - mu), (x[i] - mu).T) / x.shape[0])
+
+        theta_0 = 0.5 * (mu_0.T @ np.linalg.inv(Sigma) @ mu_0 - mu_1.T @ np.linalg.inv(Sigma) @ mu_1) - np.log(phi_0 / phi_1)
+        theta = -np.linalg.inv(Sigma) @ (mu_0 - mu_1)
+        self.theta = np.append(theta_0, theta)
+        # *** END CODE HERE ***
+
+    def predict(self, x):
+        """Make a prediction given new inputs x.
+
+        Args:
+            x: Inputs of shape (n_examples, dim).
+
+        Returns:
+            Outputs of shape (n_examples,).
+        """
+        # *** START CODE HERE ***
+        return 1 / (1 + np.exp(-((x.dot(self.theta[1:].T))+ self.theta[0])))
+        # *** END CODE HERE
+
+if __name__ == '__main__':
+    main('experiments/test')
--- src/util/plots.py
+++ src/util/plots.py
@@ -1,28 +1,32 @@
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+import itertools
 
 def sigmoid(z):
     return 1 / (1 + np.exp(-z))
 
 def plot(X: np.ndarray,
+         Y: np.ndarray,
          thetas: np.ndarray, 
          save_path: str,
          correction=1.0):
-    """
-    Args:
-        x: input examples, (n, d)
-        y: labels, (n,)
-        thetas: weights, (,d)
-    """
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
-    y = sigmoid(X @ thetas) > 0.5
     # Color based on the fourth feature
-    colors = X[:, 3]
+    # Use the fourth dimension (colors) as the colormap
+    colors = np.array(X[:, 3])
+    
+    # Normalize colors to map to the entire color spectrum
+    norm = plt.Normalize(colors.min(), colors.max())
+    cmap = plt.cm.viridis  # You can choose a different colormap
 
-    # Scatter plot
-    sc = ax.scatter(X[:, 0], X[:, 1], X[:, 2], c=colors, cmap=plt.hot())
-    plt.colorbar(sc)
+    sc = ax.scatter(X[Y.ravel() == 0, 0], X[Y.ravel() == 0, 1], X[Y.ravel() == 0, 2], c=colors[Y.ravel() == 0], cmap=cmap, marker='o', norm=norm, label='Y=0 (Circles)')
+    sc = ax.scatter(X[Y.ravel() == 1, 0], X[Y.ravel() == 1, 1], X[Y.ravel() == 1, 2], c=colors[Y.ravel() == 1], cmap=cmap, marker='s', norm=norm, label='Y=1 (Squares)')
+
+    plt.xlabel('X1')
+    plt.ylabel('X2')
+    ax.set_zlabel('X3')
 
     # Creating a meshgrid for the decision boundary visualization
     x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
@@ -34,6 +38,79 @@ def plot(X: np.ndarray,
     zz = -(thetas[0] * xx + thetas[1] * yy + thetas[3] * np.mean(colors)) / thetas[2]
 
     # Decision boundary plot
-    ax.plot_surface(xx, yy, zz, color='c', alpha=0.2)
+    ax.plot_surface(xx, yy, zz, color='c', alpha=0.2, label='Decision Boundary')
+    cbar = plt.colorbar(sc)
+    cbar.set_label('X4')
 
     plt.show()
+
+def plot_with_pca(X: np.ndarray,
+                  Y: np.ndarray,
+                  thetas: np.ndarray, 
+                  save_path: str,
+                  correction=1.0):
+    fig, ax = plt.subplots()
+
+    # Apply PCA to reduce dimensionality to 2 dimensions
+    pca = PCA(n_components=2)
+    reduced_X = pca.fit_transform(X)
+
+    # Use the reduced data for plotting
+    reduced_colors = reduced_X[:, 1]  # You can choose any dimension as the color
+    
+    # Scatter plot with color mapping
+    sc = ax.scatter(reduced_X[Y.ravel() == 0, 0], reduced_X[Y.ravel() == 0, 1], c=reduced_colors[Y.ravel() == 0], cmap=plt.cm.viridis, marker='o', label='Y=0 (Circles)')
+    sc = ax.scatter(reduced_X[Y.ravel() == 1, 0], reduced_X[Y.ravel() == 1, 1], c=reduced_colors[Y.ravel() == 1], cmap=plt.cm.viridis, marker='s', label='Y=1 (Squares)')
+
+    plt.xlabel('PCA Dimension 1')
+    plt.ylabel('PCA Dimension 2')
+
+    # To plot the decision boundary in 2D
+    x_min, x_max = reduced_X[:, 0].min() - 1, reduced_X[:, 0].max() + 1
+    y_min, y_max = reduced_X[:, 1].min() - 1, reduced_X[:, 1].max() + 1
+    xx, yy = np.meshgrid(np.linspace(x_min, x_max, 100),
+                         np.linspace(y_min, y_max, 100))
+    
+    Z = thetas[0] + thetas[1] * xx + thetas[2] * yy
+    ax.contour(xx, yy, Z, [0], colors='c', linewidths=0.5)
+
+    # Create colorbar for the reduced color dimension
+    cbar = plt.colorbar(sc)
+    cbar.set_label('PCA Color')
+
+    ax.legend()
+    plt.show()
+
+def plot_all_feature_pairs(x, y, theta, save_path, correction=1.0):
+    """Plot dataset and fitted logistic regression parameters.
+
+    Args:
+        x: Matrix of training examples, one per row.
+        y: Vector of labels in {0, 1}.
+        theta: Vector of parameters for logistic regression model.
+        save_path: Path to save the plot.
+        correction: Correction factor to apply, if any.
+    """
+    # Plot dataset
+    features = [i for i in range(x.shape[1])]
+    all_feature_pairs = list(itertools.combinations(features, 2))
+    for (feat_1, feat_2) in all_feature_pairs:
+        x_copy = x[:, [feat_1, feat_2]]
+        print(y.shape)
+        theta_copy = [theta[feat_1], theta[feat_2]]
+        plt.figure()
+        plt.plot(x_copy[y == 1, -2], x_copy[y == 1, -1], 'bx', linewidth=2)
+        plt.plot(x_copy[y == 0, -2], x_copy[y == 0, -1], 'go', linewidth=2)
+
+        # Plot decision boundary (found by solving for theta^T x = 0)
+        x1 = np.arange(min(x[:, -2]), max(x[:, -2]), 0.01)
+        x2 = -(theta[0] / theta_copy[2] + theta_copy[1] / theta_copy[2] * x1
+            + np.log((2 - correction) / correction) / theta_copy[2])
+        plt.plot(x1, x2, c='red', linewidth=2)
+        plt.xlim(x[:, -2].min()-.1, x[:, -2].max()+.1)
+        plt.ylim(x[:, -1].min()-.1, x[:, -1].max()+.1)
+
+        # Add labels and save to disk
+        plt.xlabel('x1')
+        plt.ylabel('x2')
+        plt.show()
