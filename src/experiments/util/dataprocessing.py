b1a7dd80b562b2528d6f7bf8a34bceb2f2d05269
Jonathan Coronado <jonathan@DN0a22b220.SUNet>
2023-12-08 01:34:51 -0800

    Add data transformations to dataprocessing

--- src/experiments/util/dataprocessing.py
+++ src/experiments/util/dataprocessing.py
@@ -3,6 +3,7 @@ import pandas as pd
 from math import isnan
 from sklearn.model_selection import train_test_split
 from sklearn.utils import resample
+from sklearn.preprocessing import PowerTransformer, StandardScaler, MinMaxScaler
 
 import sys
 def add_intercept(x):
@@ -35,6 +36,11 @@ def load_dataset(filename, position, add_intercept):
         global add_intercept
         return add_intercept(x)
 
+
+    scaler = PowerTransformer(method='yeo-johnson')  # or method='box-cox'
+    standard_scaler = StandardScaler()
+    min_max_scaler = MinMaxScaler()
+
     # Enumerate the features we want
     allowed_col_labels = ['ProjRushAtt','ProjRushYd','RushingYardsPerAttempt','ProjRushTD','ReceivingTargets','ProjRecCount','ProjRecYd','ProjRecTD','Fumbles','FumblesLost','DiffPPR1']
 
@@ -46,11 +52,6 @@ def load_dataset(filename, position, add_intercept):
 
     # Change last column to labels
     new_df['DiffPPR1'] = new_df['DiffPPR1'].apply(lambda x: 0 if x < 0 else 1)
-    
-
-    # Replace any NaN values with 0
-    for col in new_df:
-        new_df[col] = new_df[col].apply(lambda x: 0 if isnan(x) or np.isinf(x) else x)
 
     new_df_0 = new_df[new_df["DiffPPR1"] == 0]
     new_df_1 = new_df[new_df["DiffPPR1"] == 1]
@@ -59,22 +60,37 @@ def load_dataset(filename, position, add_intercept):
 
     new_df = pd.concat([df_class_0_bal, new_df_1])
 
-
     new_df_0 = new_df[new_df["DiffPPR1"] == 0]
     new_df_1 = new_df[new_df["DiffPPR1"] == 1]
 
+    # # Log Scaling
+    # log_scaled = np.log1p(new_df.iloc[:,:-1])
+
+    # Yeo-Johnson transformation
+    yeo_johnson_transformed = scaler.fit_transform(new_df.iloc[:,:-1])
+
+    # Standard Scaler
+    standard_scaled = standard_scaler.fit_transform(yeo_johnson_transformed)
+
+    # Min-Max Scaler
+    min_max_scaled = min_max_scaler.fit_transform(standard_scaled)
+
+    # Recombine with the labels
+    new_df.iloc[:,:-1] = yeo_johnson_transformed
+    
     # Splitting 80% for training and 20% for temp (which will be further split)
     train_df, temp_df = train_test_split(new_df, test_size=0.2, random_state=42, stratify=new_df["DiffPPR1"])
 
     # Splitting the temp_df into 50% validation and 50% test (which results in 10% of original data for both)
     valid_df, test_df = train_test_split(temp_df, test_size=0.5, random_state=42, stratify=temp_df["DiffPPR1"])
 
+
     x_train, y_train = train_df.iloc[:,:-1].values, train_df.iloc[:, [-1]].values
     x_valid, y_valid = valid_df.iloc[:,:-1].values, valid_df.iloc[:, [-1]].values
     x_test, y_test = test_df.iloc[:,:-1].values, test_df.iloc[:, [-1]].values
     # Isolate and return features and labels (x_train, y_train, x_valid, y_valid, x_test, y_test)
 
-    if add_intercept == True:
+    if add_intercept:
         x_train = add_intercept_fn(np.array(x_train))
         x_valid = add_intercept_fn(np.array(x_valid))
         x_test = add_intercept_fn(np.array(x_test))
